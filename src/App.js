a4ed78cfe6119139fba145782c33a1a5b88e7f61
Debleena18 <sarkardebleena18@gmail.com>
2022-04-16 16:08:06 +0530

    Header and footer done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import './App.css';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Header from './components/Header/Header';
+import SimpleBottomNavigation from "./components/MainNav"
+import { Container } from '@material-ui/core';
+ 
 
 function App() {
   return (
-    <div className="App"></div>
+    //It is used to wrap up all the application that is going to be inside of our router of React Router Dom.
+    <BrowserRouter> 
+      <Header />
+    <div className="app">
+    <Container>
+      <Switch>
+        <Route path= '/' component= {Trending} exact /> //Keyword "Exact" because if "/" route is inside other routes it doesn't overlap.
+        <Route path= '/movies' component= {Movies} />
+        <Route path= '/series' component= {Series} />
+        <Route path= '/search' component= {Search} />
+      </Switch>
+    </Container>
+    </div>
+    <SimpleBottomNavigation />
+    </BrowserRouter>
   );
 }
 
