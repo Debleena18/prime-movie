1578a8d5cf68b4a0f07d29cc57c6c9cc71632faa
Debleena18 <sarkardebleena18@gmail.com>
2022-04-16 16:57:49 +0530

    all routing pages

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import { BrowserRouter, Route, Routes  } from "react-router-dom";
 import Header from './components/Header/Header';
 import SimpleBottomNavigation from "./components/MainNav"
 import { Container } from '@material-ui/core';
-import Trending from './components/Pages/Trending/Trending';
-import Movies from './components/Pages/Movies/Movies';
-import Search from './components/Pages/Search/Search';
-import Series from './components/Pages/Series/Series';
+import Trending from './Pages/Trending/Trending'
+import Movies from './Pages/Movies/Movies'
+import Search from './Pages/Search/Search';
+import Series from './Pages/Series/Series';
  
 // BrowserRouter is used to wrap up all the application that is going to be inside of our router of React Router Dom.
 //New Declaration of Routes is given below.
