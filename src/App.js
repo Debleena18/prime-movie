cbe2815a53ada0060bf9d54d1f656b6ec3820d83
Debleena18 <sarkardebleena18@gmail.com>
2022-04-16 16:38:41 +0530

    Switch changes to Routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import './App.css';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Routes  } from "react-router-dom";
 import Header from './components/Header/Header';
 import SimpleBottomNavigation from "./components/MainNav"
 import { Container } from '@material-ui/core';
+import Trending from './components/Pages/Trending/Trending';
+import Movies from './components/Pages/Movies/Movies';
+import Search from './components/Pages/Search/Search';
+import Series from './components/Pages/Series/Series';
  
-
+// BrowserRouter is used to wrap up all the application that is going to be inside of our router of React Router Dom.
+//New Declaration of Routes is given below.
 function App() {
   return (
-    //It is used to wrap up all the application that is going to be inside of our router of React Router Dom.
     <BrowserRouter> 
       <Header />
     <div className="app">
     <Container>
-      <Switch>
-        <Route path= '/' component= {Trending} exact /> //Keyword "Exact" because if "/" route is inside other routes it doesn't overlap.
-        <Route path= '/movies' component= {Movies} />
-        <Route path= '/series' component= {Series} />
-        <Route path= '/search' component= {Search} />
-      </Switch>
+      <Routes >
+        <Route path= '/' element= {<Trending />} /> 
+        <Route path= '/movies' element= {<Movies />} />
+        <Route path= '/series' element= {<Series />} />
+        <Route path= '/search' element= {<Search />} />
+      </Routes >
     </Container>
     </div>
     <SimpleBottomNavigation />
