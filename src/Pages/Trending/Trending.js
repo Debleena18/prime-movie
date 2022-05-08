d42f1a6ed9286749e29a1ca277a53ba760b73ecd
Debleena18 <sarkardebleena18@gmail.com>
2022-05-08 11:08:29 +0530

    few changes

--- src/Pages/Trending/Trending.js
+++ src/Pages/Trending/Trending.js
@@ -1,7 +1,40 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import SingleContent from '../../components/SingleContent/SingleContent';
+
 const Trending = () => {
+
+    const [content, setContent] = useState([])
+    const fetchTrending = async() => {
+        const { data } = await axios.get(`https://api.themoviedb.org/3/movie/550?api_key=36f794e9479b36c8af2c5825fd6caf32`);
+
+        setContent(data.results);
+    };
+
+    useEffect(() => {
+        //window.scroll(0, 0);
+        fetchTrending();
+        // eslint-disable-next-line
+      }, []);
+
     return (
         <div>
             <span className="pageTitle">Trending</span>
+            <div className='trending'>
+                {
+                    content && content.map((c) => (
+                        <SingleContent 
+                        key={c.id} 
+                        id={c.id} 
+                        poster={c.poster_path} 
+                        title={c.title || c.name} 
+                        date={c.first_air_date || c.release_date} 
+                        media_type={c.media_type}
+                        vote_average={c.vote_average}
+                        />
+                    ))
+                }
+            </div>
         </div>
     )
 }
